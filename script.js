781bf9d195c43eea0dc9a7add5c8a08a276fbd97
herman-23 <hermansood23@gmail.com>
2024-10-21 17:00:08 -0700

    Made a personal portfolio project which will contain other projects I have done soon

---
+++ script.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".multiple-text", {
+    strings: ["Web Developer", "Front-End Developer","Martial Artist", "Gamer", "Game Design Enthusiast"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
